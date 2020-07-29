2b50dbed27c2f6209c486c5d3033101fbe48634c
Fabian <FabianMahling@web.de>
2020-07-29 21:55:27 +0200

    comment on 2

--- uebung7/hashsearch.cpp
+++ uebung7/hashsearch.cpp
@@ -8,12 +8,10 @@ const int64_t m = 1e9 + 9; // magnitude hash table
 
 void nextHash(int64_t &hash, const int64_t &p_pow, const char &c_old, const char &c_new)
 {
-    // TODO: compute next rolling hash based on previous hash, p_pow, the
-    // character "leaving" the search window (c_old) and the character
-    // "entering" the search window (c_new).
-    
+    // char leaving the window:
     hash = (hash - ((p_pow * c_old) % m)) % m;
     hash = (hash < 0) ? hash+m : hash;
+    // char entering the window:
     hash = (hash * p) % m;
     hash = (hash + ((p * c_new) % m)) % m;
 }
@@ -29,15 +27,13 @@ std::vector<int> search(std::string pattern, std::string text)
     int64_t patternLength = pattern.length();
     int64_t textLength = text.length();
 
-    // TODO: compute p_pow = p ^ patternLength (modulo m).
-    // keep in mind to apply modulo after each operation.
-    // hint: you can't simply use std::pow because of that.
+    // computation of p_pow based on size of searched string:
     int64_t p_pow = 1;
     for(int i = 0; i < patternLength; i++) {
         p_pow = (p_pow * p) % m;
     }
 
-    // TODO: compute patternHash, the hash value of the pattern and the first
+    // compute patternHash, the hash value of the pattern and the first
     // substringHash.
     int64_t patternHash = 0;
     int64_t substringHash = 0;
@@ -47,13 +43,12 @@ std::vector<int> search(std::string pattern, std::string text)
         substringHash = (((substringHash + text[i]) % m ) * p )%m;
     }
     
-    // TODO: compare each individual substring's hash with patternHash and push
-    // all found occurrences to the positions vector.
-    // compute the next substringHash if needed.
+    // this could perhaps be moved into the loop, checks the first generated hashs:
     if(substringHash == patternHash){
         positions.push_back(0);
     }
     
+    // loop through the remainig chars and check if equal to searched string:
     for(int i = 1; i < textLength-patternLength; i++){
         nextHash(substringHash, p_pow, text[i-1],text[i+patternLength-1]);
         if(substringHash == patternHash){
